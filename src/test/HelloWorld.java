685aeed8872d2e8c77fce7192698fda50eddf12f
zhiyuzhang86 <zhang.zhiyu86@gmail.com>
2012-05-14 10:36:45 -0400

    Ant Test
    test

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zzhang
+ * Date: May 14, 2012
+ * Time: 9:29:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWord !!");
+	}
+}
