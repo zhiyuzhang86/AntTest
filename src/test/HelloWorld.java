f32ad3cff7be2953f798c38c1f19e31f74e8e264
zhiyuzhang86 <zhang.zhiyu86@gmail.com>
2012-05-14 10:42:04 -0400

    change to zack

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -11,6 +11,6 @@ package test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWord !!");
+		System.out.println("Hello Zack!!");
 	}
 }
